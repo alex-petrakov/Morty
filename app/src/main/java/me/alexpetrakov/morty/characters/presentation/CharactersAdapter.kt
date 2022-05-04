52de13bde0cee6cf447088ca5309e515fd61bfc1
Alex Petrakov <alex.petrakov.dev@gmail.com>
2022-05-04 20:16:04 +0300

    Load and display character avatars in CharactersAdapter

--- app/src/main/java/me/alexpetrakov/morty/characters/presentation/CharactersAdapter.kt
+++ app/src/main/java/me/alexpetrakov/morty/characters/presentation/CharactersAdapter.kt
@@ -2,17 +2,21 @@ package me.alexpetrakov.morty.characters.presentation
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import me.alexpetrakov.morty.databinding.ItemCharacterBinding
 
-class CharactersAdapter :
-    ListAdapter<CharacterUiModel, CharactersAdapter.ViewHolder>(CharacterUiModel.DiffUtilCallback) {
+class CharactersAdapter(
+    private val fragment: Fragment
+) : ListAdapter<CharacterUiModel, CharactersAdapter.ViewHolder>(CharacterUiModel.DiffUtilCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ItemCharacterBinding.inflate(layoutInflater, parent, false)
-        return ViewHolder(binding)
+        return ViewHolder(binding, fragment)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -20,9 +24,15 @@ class CharactersAdapter :
     }
 
     class ViewHolder(
-        private val binding: ItemCharacterBinding
+        private val binding: ItemCharacterBinding,
+        private val fragment: Fragment
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(character: CharacterUiModel): Unit = with(binding) {
+            Glide.with(fragment)
+                .load(character.imageUrl)
+                .circleCrop()
+                .transition(withCrossFade())
+                .into(avatarImageView)
             nameTextView.text = character.name
             descriptionTextView.text = character.description
         }
--- app/src/main/java/me/alexpetrakov/morty/characters/presentation/CharactersFragment.kt
+++ app/src/main/java/me/alexpetrakov/morty/characters/presentation/CharactersFragment.kt
@@ -19,7 +19,7 @@ class CharactersFragment : Fragment() {
 
     private val binding get() = _binding!!
 
-    private val charactersAdapter = CharactersAdapter()
+    private val charactersAdapter = CharactersAdapter(this)
 
     override fun onCreateView(
         inflater: LayoutInflater,
